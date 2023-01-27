be607260dfeed876724dde48f6c520a1172bb8f6
sen-ol <senolevsen@gmail.com>
2023-01-27 16:29:42 +0300

    3. ders 11dk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
+import Test from './Test';
+import { Title } from "./Components";
+import Bootstrap from './Bootstrap';
+
+import styles from './App.module.css'
 
 function App() {
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      // analytici bilgilendir
+    }
+  })
+
+
   return (
-    <div className="App">
-     <img src='/logo192.png' />
-     <img src={logo} />
+    <div className={styles.App}>
+      <Title>{process.env.NODE_ENV}</Title>
+      <Title theme="dark">{process.env.NODE_ENV}</Title>
+      <p>
+        {process.env.REACT_APP_API_URL}  
+      </p>
+      <Test />
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <img src='/logo192.png' />
+          <img src={logo} />
+        </>
+      )}
+     
+        <Bootstrap />
+
     </div>
   );
 }
---
+++ src/Bootstrap.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap'
+
+export default function Bootstrap() {
+  return (
+    <div>
+        <h3>Bootstrap Componentleri</h3>
+        <Button>Buton</Button>
+        <Button variant='success'>Buton</Button>
+    </div>
+  )
+}
